ef7d5f60654e8ad6c9431ec0261ec8055300ea96
tree105778 <bjyon0415@gmail.com>
2025-04-20 15:17:10 +0900

    lint stage 설정 완료

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -5,7 +5,9 @@ const buildEslintCommand = (filenames) =>
     .map((f) => path.relative(process.cwd(), f))
     .join(' --file ')}`;
 
+const buildPrettierCommand = (filenames) =>
+  `prettier --write ${filenames.map((f) => path.resolve(f)).join(' ')}`;
+
 module.exports = {
-  '**.*.{js,jsx,ts,tsx}': ['prettier --write', buildEslintCommand],
-  '**.*.{json.css,scss,md}': ['prettier --write'],
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand, buildPrettierCommand],
 };
