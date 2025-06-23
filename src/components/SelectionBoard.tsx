7f7d76d7024892ad25f7ddddccf2996785e55d41
tree105778 <bjyon0415@gmail.com>
2025-06-24 00:52:55 +0900

    인덱스 페이지 select 부분을 tab으로 수정

--- src/components/SelectionBoard.tsx
+++ src/components/SelectionBoard.tsx
@@ -1,15 +1,8 @@
 import { FetchChampions, FetchItems } from '@/types';
 import styles from './SelectionBoard.module.css';
-import { useState } from 'react';
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select';
 import ChampionSelectBoard from '@/components/ChampionSelectBoard';
 import ItemSelectBoard from '@/components/ItemSelectBoard';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 export default function SelectionBoard({
   champions,
@@ -18,37 +11,44 @@ export default function SelectionBoard({
   champions: FetchChampions[] | null;
   items: FetchItems[] | null;
 }) {
-  const [selectOpt, setSelectOpt] = useState('champion');
-
   return (
     <div className={styles.boardWrapper}>
       <div className={styles.boardHeaderSection}>
-        <h1>QUICK PICK</h1>
-        <div className={styles.boardHeaderSelect}>
-          <Select
-            value={selectOpt}
-            onValueChange={(value) => setSelectOpt(value)}
-          >
-            <SelectTrigger className="border-none focus-visible:ring-transparent !text-[1rem] !h-[1.5rem] !w-[content] font-bold">
-              <SelectValue placeholder="챔피언" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="champion">챔피언</SelectItem>
-              <SelectItem value="item">아이템</SelectItem>
-              <SelectItem value="meta">추천메타</SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
-        {selectOpt === 'champion' ? (
-          <p>챔피언별 덱 추천</p>
-        ) : (
-          <p>아이템별 덱 추천</p>
-        )}
+        <h1 className="!text-white !text-[1.8rem] font-bold">QUICK PICK</h1>
+        <p className="!ml-3">상황별 덱 추천</p>
       </div>
-      {selectOpt === 'champion' && champions && (
-        <ChampionSelectBoard champions={champions} />
-      )}
-      {selectOpt === 'item' && items && <ItemSelectBoard items={items} />}
+      <Tabs defaultValue="champion">
+        <TabsList className="!mx-auto rounded-none !border-b-2 !border-b-[#2C2C2C] gap-16 !mt-6 !mb-4 !pb-4">
+          <TabsTrigger
+            className="text-[1.5rem] px-0 data-[state=active]:text-[#06F1D2] data-[state=active]:underline underline-offset-17"
+            value="champion"
+          >
+            챔피언
+          </TabsTrigger>
+          <TabsTrigger
+            className="text-[1.5rem] px-0 data-[state=active]:text-[#06F1D2] data-[state=active]:underline underline-offset-17"
+            value="item"
+          >
+            아이템
+          </TabsTrigger>
+          <TabsTrigger
+            className="text-[1.5rem] px-0 data-[state=active]:text-[#06F1D2] data-[state=active]:underline underline-offset-17"
+            value="meta"
+          >
+            추천 메타
+          </TabsTrigger>
+        </TabsList>
+        {champions && (
+          <TabsContent value="champion">
+            <ChampionSelectBoard champions={champions} />
+          </TabsContent>
+        )}
+        {items && (
+          <TabsContent value="item">
+            <ItemSelectBoard items={items} />
+          </TabsContent>
+        )}
+      </Tabs>
     </div>
   );
 }
