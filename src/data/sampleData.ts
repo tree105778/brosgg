90d9ac2a221247fda40d2a0dbb82eaaeb82ffcb0
tree105778 <bjyon0415@gmail.com>
2025-10-30 14:03:05 +0900

    chore: 페이지 작업용 임시 샘플 데이터 추가

---
+++ src/data/sampleData.ts
@@ -0,0 +1,228 @@
+import { TFTMetaPanelProp } from '@/types/prop';
+
+export const sampleRecommendItemData = {
+  items: [
+    {
+      itemName: '피바라기',
+      itemImgSrc:
+        'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+    },
+    {
+      itemName: '피바라기',
+      itemImgSrc:
+        'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+    },
+    {
+      itemName: '피바라기',
+      itemImgSrc:
+        'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+    },
+  ],
+  avgPlacement: 3.5,
+  top4Rate: 48.7,
+  firstRate: 21.3,
+  gameCount: 987,
+};
+
+export const sampleTFTMetaData: TFTMetaPanelProp = {
+  metaTier: 'S',
+  metaName: '미포 테크 덱',
+  champs: [
+    {
+      cost: 5,
+      name: '리 신',
+      image:
+        'https://cdn.lolchess.gg/upload/images/champions/LeeSin_1752221994-LeeSin.jpg',
+      items: [
+        {
+          itemName: '피바라기',
+          itemImgSrc:
+            'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+        },
+        {
+          itemName: '피바라기',
+          itemImgSrc:
+            'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+        },
+      ],
+    },
+    {
+      cost: 5,
+      name: '리 신',
+      image:
+        'https://cdn.lolchess.gg/upload/images/champions/LeeSin_1752221994-LeeSin.jpg',
+      items: [
+        {
+          itemName: '피바라기',
+          itemImgSrc:
+            'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+        },
+        {
+          itemName: '피바라기',
+          itemImgSrc:
+            'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+        },
+      ],
+    },
+    {
+      cost: 5,
+      name: '리 신',
+      image:
+        'https://cdn.lolchess.gg/upload/images/champions/LeeSin_1752221994-LeeSin.jpg',
+      items: [
+        {
+          itemName: '피바라기',
+          itemImgSrc:
+            'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+        },
+        {
+          itemName: '피바라기',
+          itemImgSrc:
+            'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+        },
+      ],
+    },
+    {
+      cost: 5,
+      name: '리 신',
+      image:
+        'https://cdn.lolchess.gg/upload/images/champions/LeeSin_1752221994-LeeSin.jpg',
+      items: [
+        {
+          itemName: '피바라기',
+          itemImgSrc:
+            'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+        },
+        {
+          itemName: '피바라기',
+          itemImgSrc:
+            'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+        },
+        {
+          itemName: '피바라기',
+          itemImgSrc:
+            'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+        },
+      ],
+    },
+    {
+      cost: 5,
+      name: '리 신',
+      image:
+        'https://cdn.lolchess.gg/upload/images/champions/LeeSin_1752221994-LeeSin.jpg',
+      items: [
+        {
+          itemName: '피바라기',
+          itemImgSrc:
+            'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+        },
+        {
+          itemName: '피바라기',
+          itemImgSrc:
+            'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+        },
+        {
+          itemName: '피바라기',
+          itemImgSrc:
+            'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+        },
+      ],
+    },
+    {
+      cost: 5,
+      name: '리 신',
+      image:
+        'https://cdn.lolchess.gg/upload/images/champions/LeeSin_1752221994-LeeSin.jpg',
+      items: [
+        {
+          itemName: '피바라기',
+          itemImgSrc:
+            'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+        },
+        {
+          itemName: '피바라기',
+          itemImgSrc:
+            'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+        },
+        {
+          itemName: '피바라기',
+          itemImgSrc:
+            'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+        },
+      ],
+    },
+    {
+      cost: 5,
+      name: '리 신',
+      image:
+        'https://cdn.lolchess.gg/upload/images/champions/LeeSin_1752221994-LeeSin.jpg',
+      items: [
+        {
+          itemName: '피바라기',
+          itemImgSrc:
+            'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+        },
+        {
+          itemName: '피바라기',
+          itemImgSrc:
+            'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+        },
+        {
+          itemName: '피바라기',
+          itemImgSrc:
+            'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+        },
+      ],
+    },
+    {
+      cost: 5,
+      name: '리 신',
+      image:
+        'https://cdn.lolchess.gg/upload/images/champions/LeeSin_1752221994-LeeSin.jpg',
+      items: [
+        {
+          itemName: '피바라기',
+          itemImgSrc:
+            'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+        },
+        {
+          itemName: '피바라기',
+          itemImgSrc:
+            'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+        },
+        {
+          itemName: '피바라기',
+          itemImgSrc:
+            'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+        },
+      ],
+    },
+    {
+      cost: 5,
+      name: '리 신',
+      image:
+        'https://cdn.lolchess.gg/upload/images/champions/LeeSin_1752221994-LeeSin.jpg',
+      items: [
+        {
+          itemName: '피바라기',
+          itemImgSrc:
+            'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+        },
+        {
+          itemName: '피바라기',
+          itemImgSrc:
+            'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+        },
+        {
+          itemName: '피바라기',
+          itemImgSrc:
+            'https://cdn.lolchess.gg/upload/images/items/Bloodthirster_1710231772-bloodthirster.png',
+        },
+      ],
+    },
+  ],
+  avgPlacement: 3.5,
+  top4Rate: 48.7,
+  firstRate: 21.3,
+  pickRate: 8.2,
+};
